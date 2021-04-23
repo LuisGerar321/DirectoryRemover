89e8e609e33b27fd9916e97a5d039cd4a8b0b74b
Luis <dojas64@gmail.com>
2021-04-23 17:11:18 -0500

    fixed bug

--- DirectoryRemover.js
+++ DirectoryRemover.js
@@ -5,37 +5,44 @@ var number_files= 0;
 var number_folders = 0;
 
 const  directoryRemover = (path) =>{
-        number_folders  = number_folders + 1;
+        
         try{
                 if(fs.existsSync(path)){
-                        const files = fs.readdirSync(path);
-                        
-                        if( files.length>0){  //folder is not empty
-                                const answer = readline.question("The directory is not empty, are you sure? yes/no\n");
-                                if(answer == "yes"){
-
-                                        files.forEach(file => {
-                                                const c_path = path + "/" + file;
-
-                                                if(fs.lstatSync(c_path).isDirectory()) {
-                                                        
-                                                        directoryRemover(c_path)
-                                                }else{
-                                                        number_files+=1;
-                                                        fs.unlinkSync(c_path);
-                                                        
-                                                }
-                                                
-                                        });
-                                        fs.rmdirSync(path);
-
-                                       
-                                }else{
-                                        console.log("element canceled");
-                                }
+                        try{
                                 
-                        }else if(files.length==0) {
-                                fs.rmdirSync(path);
+                                const files = fs.readdirSync(path);
+                                number_folders  = number_folders + 1;
+                                if( files.length>0){  //folder is not empty
+                                        const answer = readline.question("The directory is not empty, are you sure? yes/no\n");
+                                        if(answer == "yes"){
+
+                                                files.forEach(file => {
+                                                        const c_path = path + "/" + file;
+
+                                                        if(fs.lstatSync(c_path).isDirectory()) {
+                                                                
+                                                                directoryRemover(c_path)
+                                                        }else{
+                                                                number_files+=1;
+                                                                fs.unlinkSync(c_path);
+                                                                
+                                                        }
+                                                        
+                                                });
+                                                fs.rmdirSync(path);
+
+                                        
+                                        }else{
+                                                console.log("element canceled");
+                                        }
+                                        
+                                }else if(files.length==0) {
+                                        fs.rmdirSync(path);
+                                }       
+                        }catch{
+
+                                fs.unlinkSync(path);
+                                number_files+=1;
                         }
                         
 
@@ -47,6 +54,14 @@ const  directoryRemover = (path) =>{
         }
 
 }
-directoryRemover("./holi");
-console.log(`Files delated: ${number_files}, Folders delated: ${number_folders}`);
+
+try{
+        console.log("Path to remove: ",`${process.argv[2]}`);
+        directoryRemover(`${process.argv[2]}`);
+        console.log(`Files delated: ${number_files}, Folders delated: ${number_folders}`);
+}catch{
+        console.log("Error");
+}
+
+
 
